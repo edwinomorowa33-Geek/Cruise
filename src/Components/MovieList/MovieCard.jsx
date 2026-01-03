1ee894e654d6a8a43616ede6d14bed4a7d802753
Omorowa Edwin <edwinomorowa33@gmail.com>
2026-01-03 13:07:58 +0100

    i guess i am up to date

--- src/Components/MovieList/MovieCard.jsx
+++ src/Components/MovieList/MovieCard.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import "./MovieCard.css";
 import dummy from "../../assets/Images/dummy.jpg";
-import Star from "../../assets/Images/star.png";
 
 const MovieCard = ({
   title = "Movie Title",
   date = "10-20-2020",
   rating = 8.0,
-  posterPath = null, // new prop for dynamic poster
+  posterPath = null,
 }) => {
-  // TMDB full poster URL or fallback
+  
   const posterUrl = posterPath
     ? `https://image.tmdb.org/t/p/w500${posterPath}`
     : dummy;
 
   return (
     <a href="#" className="movie_card">
-      <img src={posterUrl} alt="Movie Poster" className="movie_poster" />
+      <img
+        src={posterUrl}
+        alt={`${title} poster`}
+        className="movie_poster"
+      />
 
       <div className="movie_details">
         <h3 className="movie_title">{title}</h3>
+
         <div className="movie_date_rating">
           <p className="movie_date">{date}</p>
-          <p className="movie_rating">
-            {rating} <img src={Star} alt="star icon" className="class_emoji" />
-          </p>
+          <p className="movie_rating">{rating}</p>
         </div>
+
         <p className="movie_description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import "./Navbar.css";
 
-import Fire from "../../assets/Images/fire.png";
-import Star from "../../assets/Images/star.png";
-import Party from "../../assets/Images/partying-face.png";
-
 const Navbar = () => {
   return (
     <nav className="navbar">
-  <h1>MovieManiac</h1>
+      <h1>Cruise Movie</h1>
 
-  <div className="navbar_links">
-    <a href="#">Popular <img className="navbar_emoji" src={Fire} alt="fire emoji" /></a>
-    <a href="#">Top Rated <img className="navbar_emoji" src={Star} alt="star emoji" /></a>
-    <a href="#">Upcoming <img className="navbar_emoji" src={Party} alt="party face emoji" /></a>
-  </div>
-</nav>
-
+      <div className="navbar_links">
+        <a href="#">Popular</a>
+        <a href="#">Top Rated</a>
+        <a href="#">Upcoming</a>
+      </div>
+    </nav>
   );
 };
 
 export default Navbar;
-
-
